e40c39c8ea676c4165693554e4bfc30d49612142
Janeth Mar <ti46481@uvp.edu.mx>
2025-05-20 13:27:44 -0600

    Constantes Colors: Variables de colores en modo claro y oscuro

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -7,20 +7,38 @@ const tintColorLight = '#0a7ea4';
 const tintColorDark = '#fff';
 
 export const Colors = {
-  light: {
+  light: { // Modo Claro
     text: '#11181C',
     background: '#fff',
     tint: tintColorLight,
     icon: '#687076',
     tabIconDefault: '#687076',
     tabIconSelected: tintColorLight,
+
+    // Colores que se utilizarán en la APP
+    colorA: '#F0F3FA',     // Gris-Blanco
+    colorB: '#D5DEEF',    // Gris
+    colorC: '#B1C9EF',    // Azul pálido
+    colorD: '#8AAEE0',    // Azul celeste
+    colorE: '#638ECB',    // Azul
+    colorF: '#395886',    // Azul marino
+
+
   },
-  dark: {
+  dark: { // Modo Oscuro
     text: '#ECEDEE',
     background: '#151718',
     tint: tintColorDark,
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
     tabIconSelected: tintColorDark,
+
+    // Colores que se utilizarán en la APP
+    colorA: '#F0F3FA',     // Gris-Blanco
+    colorB: '#D5DEEF',    // Gris
+    colorC: '#B1C9EF',    // Azul pálido
+    colorD: '#8AAEE0',    // Azul celeste
+    colorE: '#638ECB',    // Azul
+    colorF: '#395886',    // Azul marino
   },
 };
